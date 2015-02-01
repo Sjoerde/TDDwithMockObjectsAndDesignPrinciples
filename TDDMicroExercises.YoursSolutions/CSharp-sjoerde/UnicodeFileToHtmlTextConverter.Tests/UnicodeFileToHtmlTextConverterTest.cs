97c16913619d948e67cdb1d19303acccd662d142
Sjoerde <sjoerdvanegmond@gmail.com>
2015-02-01 17:47:06 +0100

    Exercise 2
    Exercise 2: Unicode file to html text converter

---
+++ TDDMicroExercises.YoursSolutions/CSharp-sjoerde/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter.Tests
+{
+    [TestClass]
+    public class UnicodeFileToHtmlTextConverterTest
+    {
+        [TestMethod]
+        public void EncodeOneLine() 
+        { 
+            // Arrange
+            var converter = new UnicodeFileToHtmlTextConverter();
+            string line = "blabla";
+
+            // Act
+            string encoded = converter.EncodeLine(line);
+
+            // Assert
+            Assert.AreEqual("blabla<br />", encoded);
+        }
+
+        [TestMethod]
+        public void EncodeTextReader()
+        {
+            // Arrange
+            var converter = new UnicodeFileToHtmlTextConverter();
+            var reader = new StringReader(string.Format("a{0}bc{0}d", Environment.NewLine));
+
+            // Act
+            string encoded = converter.ConvertToHtml(reader);
+
+            // Assert
+            Assert.AreEqual("a<br />bc<br />d<br />", encoded);
+        }
+    }
+}
